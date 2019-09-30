b21af5ff1673ae844bd0faa9c9c76c3f280caca1
lbrenna6 <46936538+lbrenna6@users.noreply.github.com>
2019-09-30 17:26:43 +0100

    Initial classes with tests and code unimplemented

---
+++ Lowest_Common_AncestorTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+import junit.framework.TestCase;
+import static org.junit.Assert.*;
+
+public class Lowest_Common_AncestorTest extends TestCase {
+	
+	 /*Tests the (Directed Acyclic) graph is correctly constructed as a binary tree*/
+	 @Test
+	    public void testBinaryTreeConstructor() 
+	    {
+	        //TODO
+	    
+	    }
+
+	    /* Tests for */
+	    @Test
+	    public void test() 
+	    {
+	        //TODO
+	    	
+	    }
+	    
+}
